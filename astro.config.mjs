da0e8f615fcf24688f93dac044c7349a2b3d4509
Christofer Enriquez <christoferen7@gmail.com>
2025-03-19 12:37:19 -0600

    chore: config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // astro.config.mjs
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 export default defineConfig({
+  output: "static",
+  site: "https://khrizenriquez.github.io/travel-countdown/",
+  base: "/travel-countdown/",
   integrations: [react()],
   vite: {
     css: {
